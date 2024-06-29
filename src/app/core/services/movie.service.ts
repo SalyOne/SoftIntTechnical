101c7d4bce4f207e883c57638e24534225f9fc3d
SalyOne <salome.batkhadze@gmail.com>
2024-06-29 23:59:21 +0400

    task2: display movies and table

--- src/app/core/services/movie.service.ts
+++ src/app/core/services/movie.service.ts
@@ -1,6 +1,5 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Movie} from "../interface/movie";
 import {Observable} from "rxjs";
 
 @Injectable({
@@ -10,7 +9,7 @@ export class MovieService {
   private http = inject(HttpClient);
   constructor() { }
 
-  get<T>(title: string): Observable<T>{
+  get<T>(title?: string): Observable<T>{
    return this.http.get<T>(`https://online-movie-database.p.rapidapi.com/title/v2/find?title=${title}`)
   }
 }
--- src/app/pages/task2/task2.component.ts
+++ src/app/pages/task2/task2.component.ts
@@ -1,30 +1,65 @@
-import {Component, inject, OnInit} from '@angular/core';
-import { HttpClientModule} from "@angular/common/http";
+import {AfterViewInit, ChangeDetectorRef, Component, inject, OnInit, ViewChild} from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
 import {MovieService} from "../../core/services/movie.service";
 import {JsonPipe} from "@angular/common";
-import {Movie} from "../../core/interface/movie";
+import {Movie, Result} from "../../core/interface/movie";
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from "@angular/material/input";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatTableDataSource, MatTableModule} from "@angular/material/table";
+import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 
 @Component({
   selector: 'app-task2',
   standalone: true,
-  imports: [HttpClientModule, JsonPipe],
+  imports: [
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
+    JsonPipe,
+    MatCardModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatFormField,
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './task2.component.html',
   styleUrl: './task2.component.scss'
 })
 export class Task2Component implements OnInit {
   movieService = inject(MovieService)
+  loading: boolean = false;
 
   movieData!: any;
+  displayedColumns: string[] = ['name', 'type', 'year', 'image'];
+  dataSource = new MatTableDataSource<Movie>();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  value: any;
 
-  getMovie() {
-    this.movieService.get<Movie>('starwars').subscribe((data: Movie) => {
-      this.movieData = data
-      console.log(data)
+  getMovieWithTitle(title?: string) {
+    this.loading = true
+    this.movieService.get<Movie>(title).subscribe((data: Movie) => {
+      this.movieData = data.results
+      this.loading = false
+      this.dataSource = new MatTableDataSource<Movie>(this.movieData);
+      this.dataSource.paginator = this.paginator;
     })
   }
-
-  ngOnInit(): void {
-    this.getMovie()
+  ngOnInit() {
+    this.loading = true
+    this.getMovieWithTitle()
+    this.dataSource.paginator = this.paginator;
   }
+
 }
+
