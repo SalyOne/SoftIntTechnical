651b27b190a07550b579f20c056640796eccbabb
SalyOne <salome.batkhadze@gmail.com>
2024-06-30 02:13:09 +0400

    Task2: small changes

--- src/app/pages/task2/task2.component.ts
+++ src/app/pages/task2/task2.component.ts
@@ -1,18 +1,16 @@
-import {AfterViewInit, ChangeDetectorRef, Component, inject, OnInit, ViewChild} from '@angular/core';
+import {Component, inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
 import {MovieService} from "../../core/services/movie.service";
-import {JsonPipe} from "@angular/common";
-import {Movie, Result} from "../../core/interface/movie";
+import {Movie} from "../../core/interface/movie";
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from "@angular/material/input";
 import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatTableDataSource, MatTableModule} from "@angular/material/table";
-import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {Subject, takeUntil} from "rxjs";
 
 
 @Component({
@@ -24,42 +22,54 @@ import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
     MatButtonModule,
     MatIconModule,
     HttpClientModule,
-    JsonPipe,
     MatCardModule,
     MatInputModule,
     ReactiveFormsModule,
     MatFormField,
     MatTableModule,
-    MatPaginatorModule,
     MatProgressSpinnerModule
   ],
   templateUrl: './task2.component.html',
   styleUrl: './task2.component.scss'
 })
-export class Task2Component implements OnInit {
+export class Task2Component implements OnInit ,OnDestroy{
   movieService = inject(MovieService)
+  sub$ = new Subject();
   loading: boolean = false;
-
   movieData!: any;
+  isValueSubmitted:boolean = false
   displayedColumns: string[] = ['name', 'type', 'year', 'image'];
   dataSource = new MatTableDataSource<Movie>();
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
   value: any;
 
   getMovieWithTitle(title?: string) {
     this.loading = true
-    this.movieService.get<Movie>(title).subscribe((data: Movie) => {
-      this.movieData = data.results
-      this.loading = false
-      this.dataSource = new MatTableDataSource<Movie>(this.movieData);
-      this.dataSource.paginator = this.paginator;
+    if (title){
+      this.isValueSubmitted = true
+    }else{
+      title = "starwars"
+    }
+    this.movieService.get<Movie>(title)
+      .pipe(takeUntil(this.sub$))
+      .subscribe((data: Movie) => {
+      if (data){
+        this.movieData = data.results
+        this.dataSource = new MatTableDataSource<Movie>(this.movieData  );
+        this.loading = false
+      }
     })
   }
   ngOnInit() {
     this.loading = true
     this.getMovieWithTitle()
-    this.dataSource.paginator = this.paginator;
   }
-
+  clear(){
+    this.value="";
+    this.isValueSubmitted = false
+  }
+  ngOnDestroy(): void {
+    this.sub$.next(null);
+    this.sub$.complete()
+  }
 }
 
