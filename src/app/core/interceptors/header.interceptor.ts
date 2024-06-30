66a9c9a96b1bb36489f0a752a9310f3b39cd9bf0
SalyOne <salome.batkhadze@gmail.com>
2024-06-30 16:40:53 +0400

    fix: api key variable

--- src/app/core/interceptors/header.interceptor.ts
+++ src/app/core/interceptors/header.interceptor.ts
@@ -1,10 +1,10 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const headerInterceptor: HttpInterceptorFn = (req, next) => {
-
+   const apiKey = "5ee124894emshb5e465e2358bed7p13972bjsn02b30afadadc"
     req = req.clone({
       setHeaders: {
-        'X-RapidAPI-Key':'5ee124894emshb5e465e2358bed7p13972bjsn02b30afadadc',
+        'X-RapidAPI-Key': apiKey,
         'x-rapidapi-host': 'online-movie-database.p.rapidapi.com'
       }
     })
